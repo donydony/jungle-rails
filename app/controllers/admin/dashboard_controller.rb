816dd5767660f15d7de60a1bd7fd80c2e40d3f37
Donny Zhao <donny.zh@hotmail.com>
2022-09-29 06:00:11 -0400

    http basic authenticate all admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: Rails.configuration.admin_products[:user_name], password: Rails.configuration.admin_products[:password]
   def show
     @products = Product.all
     @categories = Category.all
